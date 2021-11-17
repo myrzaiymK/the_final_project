50238d0655cb246b741277e2dcec53a05d8c867c
myrzaiymK <myrzaiymkdrmtva@gmail.com>
2021-11-17 22:28:18 +0600

    little_changes

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -5,3 +5,9 @@ class LogisticAllSerializers(serializers.ModelSerializer):
     class Meta:
         model = Logistic
         fields = "__all__"
+
+
+class HitCountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HitCount
+        fields = ('visits',)
--- logistic/views.py
+++ logistic/views.py
@@ -1,14 +1,26 @@
 from django.db.models import query
-from rest_framework import generics, permissions
+from requests import Response
+from rest_framework import generics, permissions, viewsets
 from .serializers import *
 from .models import Logistic
 from .permissions import *
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from django.shortcuts import render
+from .models.db import F
 
 def oauth(request):
     return render(request, 'oauth.html')
+
+
+class HitCountViewSet(viewsets.ModelViewSet):
+    queryset = HitCount.objects.all()
+    serializer_class = HitCountSerializer
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        HitCount.objects.filter(pk=instance.id).update(visits=F('visits') + 1)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
 
 class LogisticCreateView(generics.CreateAPIView):
